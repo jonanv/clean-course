dcea72177c6ab52d5477c11cae59274983350eba
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-01-08 20:46:09 -0500

    refactor: :recycle: apply refactor exercise 5 dry

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -8,11 +8,30 @@ class Product {
     public size: Size = '',
   ) { }
 
+  isProductReady(): boolean {
+    for (const key in this) {
+      switch(typeof this[key]) {
+        case 'string':
+          if ((<string><unknown>this[key]).length <= 0) throw Error(`${ key } is empty`);
+          break;
+        case 'number':
+          if ((<number><unknown>this[key]) <= 0) throw Error(`${ key } is zero`);
+          break;
+        default:
+          throw Error(`${ typeof this[key] } is not support.`);
+      }
+    }
+    return true;
+  }
+
   toString() {
-    if (this.name.length <= 0) throw Error('name is empty');
-    if (this.price <= 0) throw Error('price is empty');
-    if (this.size.length <= 0) throw Error('size is empty');
-    
+    // No Dry
+    // if (this.name.length <= 0) throw Error('name is empty');
+    // if (this.price <= 0) throw Error('price is zero');
+    // if (this.size.length <= 0) throw Error('size is empty');
+
+    // Dry
+    if(!this.isProductReady()) return;
     return `${ this.name } $(${ this.price }), ${ this.size } `;
   }
 }
