45638650c9c0d75c8e86180087da0483c334aa0f
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-01-14 13:13:14 -0500

    feat: :sparkles: inversion de dependencias solid

---
+++ src/solid/05-dependency-a.ts
@@ -0,0 +1,18 @@
+import { PostService } from './05-dependency-b';
+import { JsonDataBaseService, LocalDataBaseService } from './05-dependency-c';
+
+
+// Main
+(async () => {
+
+  const provider = new LocalDataBaseService();
+  // const provider = new JsonDataBaseService();
+
+  const postService = new PostService(provider);
+
+  const posts = await postService.getPosts();
+
+  console.log({ posts })
+
+
+})();
