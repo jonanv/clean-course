35b1a2125fc02db055636e359d3fcf497ad4c8e8
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-01-13 23:29:34 -0500

    feat: :sparkles: principio de sustitucion de liskov

---
+++ src/solid/03-liskov-b.ts
@@ -0,0 +1,53 @@
+
+export abstract class Vehicle {
+  // getNumberOfAudiSeats(): number {
+  //   throw Error('Method not implemented');
+  // };
+
+  abstract getNumberOfSeats(): number;
+}
+
+
+export class Tesla extends Vehicle {
+
+  constructor(private numberOfSeats: number) {
+    super();
+  }
+
+  getNumberOfSeats() {
+    return this.numberOfSeats;
+  }
+}
+
+export class Audi extends Vehicle {
+
+  constructor(private numberOfSeats: number) {
+    super();
+  }
+
+  getNumberOfSeats() {
+    return this.numberOfSeats;
+  }
+}
+
+export class Toyota extends Vehicle {
+
+  constructor(private numberOfSeats: number) {
+    super();
+  }
+
+  getNumberOfSeats() {
+    return this.numberOfSeats;
+  }
+}
+
+export class Honda extends Vehicle {
+
+  constructor(private numberOfSeats: number) {
+    super();
+  }
+
+  getNumberOfSeats() {
+    return this.numberOfSeats;
+  }
+}
